e57ddddfee8d4b1650b3d0b9c80adf0c424bf6ae
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2021-08-14 12:54:58 -0700

    remove extra space

--- src/main/java/com/coolspy3/shortcommands/HelpCommand.java
+++ src/main/java/com/coolspy3/shortcommands/HelpCommand.java
@@ -15,7 +15,7 @@ public class HelpCommand {
             ShortCommands.sendMessage(TextFormatting.BLUE + justABunchOfDashes);
             ShortCommands.sendMessage(TextFormatting.YELLOW + "/sc set <trigger> <command>" + TextFormatting.AQUA + " - Sets the message \"<trigger>\" to instead type \"<command>\"");
             ShortCommands.sendMessage(TextFormatting.YELLOW + "/sc remove <trigger>" + TextFormatting.AQUA + " - Removes the specified trigger from the command list");
-            ShortCommands.sendMessage(TextFormatting.YELLOW + "/sc list"  + TextFormatting.AQUA + " - Lists the current triggers and their respective commands");
+            ShortCommands.sendMessage(TextFormatting.YELLOW + "/sc list" + TextFormatting.AQUA + " - Lists the current triggers and their respective commands");
             ShortCommands.sendMessage(TextFormatting.YELLOW + "/sc help" + TextFormatting.AQUA + " - Prints this help message");
             ShortCommands.sendMessage(TextFormatting.YELLOW + "/schelp" + TextFormatting.AQUA + " - Prints this help message");
             ShortCommands.sendMessage(TextFormatting.BLUE + justABunchOfDashes);
